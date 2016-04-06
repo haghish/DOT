8d408c9f94a8f58d2f271a0315163182f6434120
haghish <haghish21@gmail.com>
2016-04-07 00:00:43 +0200

    adding the 'return = "auto"' argument

--- R/dot.R
+++ R/dot.R
@@ -1,12 +1,14 @@
 #' @title Render and Export DOT Plots in R
-#' @usage dot(DOT, file = NULL, return = NULL)
+#' @usage dot(DOT, file = NULL, return = "auto")
+#'
+#' @keywords graphics plot diagram literate visualization
 #' @description DOT (graph description language) is a simplified and intuitive plain text graphical language. This package not only renders the DOT markup language in R, but also provides the possibility to export the graphs in PostScript and SVG (Scalable Vector Graphics) formats. The DOT package also supports literate programming packages such as Knitr and R2HTML. Visit \url{http://haghish.com/dot} for downloading examples of creating algorithms and several graphs for \code{Rmarkdown} and \code{R HTML} to generate dynamic procedural graphs in dynamic documents using the \code{DOT} package.
 #'
 #' @param DOT DOT (graph description language) is a simplified and intuitive plain text graphical language.This argument takes a string containing the DOT script. It is advised to use single quotation mark for the DOT string since the script often includes double quotations which can crash the function.
 #'
 #' @param file defines the file name for exporting the graph. The acceptable file extensions are \code{"ps"} for PostScript and \code{"svg"} for SVG format (see examples below).
 #'
-#' @param return specifies if PS or SVG script should be printed in R console. The acceptable values are \code{"verbatim"} and \code{"cat"}. \code{"verbatim"} returns a single string which is assignable to an object. \code{"cat"} returns a concatenated string which is printed in multiple lines in the R console. Use the \code{"cat"} to append SVG graphics directly in dynamic document packages such as \code{knitr, Rmarkdown}, etc.
+#' @param return specifies if PS or SVG script should be printed in R console. The acceptable values are \code{"auto"}, \code{"cat"}, \code{"verbatim"}, and \code{NULL}. The default value is \code{"auto"} which does not return anything unless the \code{dot()} function is called within \code{'knitr'} or \code{'rmarkdown'} packages, which require concatenated graphical script. The \code{"cat"} returns concatenated PS or SVG script, printed in multiple lines in the R console. The \code{"verbatim"} returns a single string which is assignable to an object.
 #'
 #' @return By default, the function only renders and loads the DOT plot in RStudio but does not return any PS or SVG script. If the \code{return} argument is specified, it returns PostScript or SVG script. Note that for assigning the script returned from \code{dot()} to an object, only \code{"verbatim"} value can be used to create a string object.
 #'
@@ -48,7 +50,7 @@
 #' @importFrom tools file_ext
 #' @importFrom utils packageVersion
 
-dot <- function(DOT, file = NULL, return = NULL) {
+dot <- function(DOT, file = NULL, return = "auto") {
 
     # ---------------------------------------------------------
     # SYNTAX PROCESSING
@@ -58,10 +60,12 @@ dot <- function(DOT, file = NULL, return = NULL) {
     #   - load V8 if there is a demand for exporting graphics
     # =========================================================
 
-    if(!requireNamespace("tools")) stop("tools package is required.", call. = FALSE)
-    requireNamespace("tools", quietly = TRUE)
+    if(!requireNamespace("tools", quietly = TRUE)) stop("tools package is required.", call. = FALSE)
+    #requireNamespace("tools", quietly = TRUE)
 
     extension <- ""
+    autoReturn <- ""
+
     if (!is.null(file)) {
         extension <- file_ext(file)
     }
@@ -72,12 +76,29 @@ dot <- function(DOT, file = NULL, return = NULL) {
         stop(err)
     }
 
-    if (!is.null(file)) {
-        if(!requireNamespace("V8")) stop("V8 package is required", call. = FALSE)
-        requireNamespace("V8", quietly = TRUE)
-        if (!(packageVersion("V8") >= "1.0")) {
-            stop('your "V8" package is too old. The DOT package requires V8
-            package version 1.0 or newer', call. = FALSE)
+
+    if(!requireNamespace("V8", quietly = TRUE)) stop("V8 package is required", call. = FALSE)
+    #requireNamespace("V8", quietly = TRUE)
+    if (!(packageVersion("V8") >= "1.0")) {
+        stop('your "V8" package is too old. The DOT package requires V8
+        package version 1.0 or newer', call. = FALSE)
+    }
+
+    # ---------------------------------------------------------
+    # CHECK THE PARENT FUNCTION
+    #   - is dot() called by 'knitr' or 'rmarkdown' ?
+    # =========================================================
+    if (return == "auto") {
+        parentName <- try(deparse(sys.calls()[[1]]), silent = TRUE)
+        PN <- try(unlist(strsplit(parentName, "::"))[[1]], silent = TRUE)
+        if (PN == "rmarkdown") {
+            autoReturn = "cat"
+        }
+        else {
+            PN <- substring(parentName, 1, 4)[[1]]
+            if (PN == "knit") {
+                autoReturn = "cat"
+            }
         }
     }
 
@@ -114,8 +135,15 @@ dot <- function(DOT, file = NULL, return = NULL) {
     JS <- system.file("lib/viz.js", package = "DOT")
     ENV <- v8();
     ENV$source(JS)
-    call <- paste("Viz(", DOT, ")", sep = "")
+    call <- paste("image = Viz(", DOT, ")", sep = "")
     content <- ENV$eval(call)
+    #print(content)
+
+    #call2 <- paste("Viz.svgXmlToPngImageElement(Viz(", DOT, "))", sep = "")
+    #content2 <- ENV$eval(call2)
+
+    #print(content2)
+
     write(content, file=dotFile, append=TRUE)
 
     viewer <- getOption("viewer")
@@ -126,6 +154,7 @@ dot <- function(DOT, file = NULL, return = NULL) {
     #    utils::browseURL(dotFile)
     #}
 
+    svgContent <- content
 
     # ---------------------------------------------------------
     # EXPORTING THE GRAPH
@@ -154,6 +183,9 @@ dot <- function(DOT, file = NULL, return = NULL) {
         }
         else if (return == "verbatim") {
             return(content)
+        }
+        else if (return == "auto" & autoReturn == "cat") {
+            cat(svgContent)       #always return SVG
         }
     }
 }
